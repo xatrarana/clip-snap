cb66e2a75da0cdc0c5fdbcad32852c1b5c924631
xatrarana <chhatraclas@gmail.com>
2025-07-23 11:16:06 +0545

    chore(setup): add prebuild script to download ffmpeg binary automatically

---
+++ binarysetup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Create bin directory if it doesn't exist
+mkdir -p ./bin
+
+# Define FFmpeg version and platform
+FFMPEG_URL="https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz"
+
+# Download and extract FFmpeg
+curl -L "$FFMPEG_URL" -o ffmpeg.tar.xz
+tar -xf ffmpeg.tar.xz
+
+# Move the ffmpeg binary to ./bin
+FFMPEG_DIR=$(tar -tf ffmpeg.tar.xz | head -1 | cut -f1 -d"/")
+mv "$FFMPEG_DIR/ffmpeg" ./bin/ffmpeg
+
+# Make it executable
+chmod +x ./bin/ffmpeg
+
+# Cleanup
+rm -rf "$FFMPEG_DIR"
+rm ffmpeg.tar.xz
+
+echo "✅ FFmpeg downloaded and placed in ./bin"
+
